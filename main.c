ffe58869ce072f83c5af0df96058f683fc7ac4b8
ebet0419-lgtm <ebet0419@sookmyung.ac.kr>
2025-11-02 22:16:08 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,23 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-    int i;
-    int grade[SIZE];
-    int score[SIZE];
+int i;
+int a[SIZE]={1,2,3,4,5};
+int b[SIZE]={1,2,3,4,5};
+int flag_same=1;
     
-    for(i=0;i<SIZE;i++)
+for(i=0;i<SIZE;i++)
     {
-                       grade[i]=rand()%100; 
-     }
-     
-    for(i=0;i<SIZE;i++)
-    {
-                       score[i]=grade[i];
-                       }
-                       
-    for (i=0;i<SIZE;i++)
-        printf("grade[%i]=%i, score[%i]=%i\n",
-        i, grade[i],i,score[i]);
+                       if (a[i]!=b[i])
+                         { printf("%i-th element is dff.\n",i);
+                          flag_same=0;
+                          }
+                          }
+printf("Are a and b the same? %i\n",flag_same);
   
   system("PAUSE");	
   return 0;
